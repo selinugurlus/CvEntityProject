34fce80b71bd0e2f617b13796bea0e9edf6804b7
selinugurlus <selinugurlu2002@hotmail.com>
2023-03-06 16:48:45 +0300

    login page ui

---
+++ CvEntityProject/web form.js
@@ -0,0 +1,19 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('app')
+        .controller('web_form', web_form);
+
+    web_form.$inject = ['$location'];
+
+    function web_form($location) {
+        /* jshint validthis:true */
+        var vm = this;
+        vm.title = 'web_form';
+
+        activate();
+
+        function activate() { }
+    }
+})();
